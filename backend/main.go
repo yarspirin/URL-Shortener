0292403716c83d99787b8ac8c90d5cc952b6a9c4
Yaroslav Spirin <yar.spirin@gmail.com>
2020-05-14 15:39:51 +0300

    Add basic backend

---
+++ backend/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(static.Serve("/", static.LocalFile("./web", true)))
+
+	api := r.Group("/api")
+	api.GET("/ping", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
+}
